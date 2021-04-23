d3877a51ef6c835a3e3a0a3937f8efae74df989a
renderz <464816158@qq.com>
2021-04-23 18:01:33 +0800

    use batchUpdate to combine formData&pagination

--- src/useFormTable.ts
+++ src/useFormTable.ts
@@ -1,10 +1,16 @@
 import { useState, useEffect, useCallback, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import { Form, TableProps, TablePaginationConfig } from 'antd';
 
 type GetTableData<Col> = (
   pagination?: TablePaginationConfig,
   formData?: Store,
 ) => Promise<{ dataSource?: Col[]; total?: number } | undefined>;
+
+type Options = {
+  isManual?: boolean;
+  shouldClean?: boolean;
+};
 
 const defaultPagination: TablePaginationConfig = {
   current: 1,
@@ -15,20 +21,26 @@ const defaultPagination: TablePaginationConfig = {
   showTotal: total => `共 ${total} 条记录`,
 };
 
-const useTableForm = <Col>(getTableData: GetTableData<Col>) => {
-  const [formData, setFormData] = useState<Store>();
-  const [pagination, setPagination] = useState<TablePaginationConfig>(defaultPagination);
-  const [total, setTotal] = useState<number | undefined>(0);
-  const [dataSource, setDataSource] = useState<Col[]>();
-
+const useTableForm = <Col>(getTableData: GetTableData<Col>, options?: Options) => {
   const [form] = Form.useForm();
 
+  // formData supposes to be undefined when initialize and becomes to be Store after submit once.
+  const [formData, setFormData] = useState<Store | undefined>(undefined);
+  const [pagination, setPagination] = useState<TablePaginationConfig>(defaultPagination);
+  const [total, setTotal] = useState<number | undefined>(0);
+  const [dataSource, setDataSource] = useState<Col[] | undefined>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
   const getTableDataRef = useRef(getTableData);
+  const optionsRef = useRef(options);
 
   const onFinish = async () => {
     const result = await form.validateFields();
-    setFormData(result);
-    setPagination(defaultPagination);
+
+    ReactDOM.unstable_batchedUpdates(() => {
+      setFormData(result);
+      setPagination(defaultPagination);
+    });
   };
 
   const onChange: TableProps<Col>['onChange'] = _pagination => {
@@ -36,32 +48,52 @@ const useTableForm = <Col>(getTableData: GetTableData<Col>) => {
   };
 
   const search = useCallback(async () => {
-    const result = await getTableDataRef.current(pagination, formData);
+    const { isManual = false, shouldClean = false } = optionsRef.current || {};
 
-    if (result) {
-      setDataSource(result.dataSource);
-      setTotal(result.total);
+    if ((isManual && formData !== undefined) || !isManual) {
+      setLoading(true);
+      if (shouldClean) {
+        setDataSource([]);
+      }
+
+      const result = await getTableDataRef.current(
+        pagination,
+        formData || form.getFieldsValue(true),
+      );
+
+      if (result) {
+        ReactDOM.unstable_batchedUpdates(() => {
+          setDataSource(result.dataSource);
+          setTotal(result.total);
+        });
+      }
+
+      setLoading(false);
     }
-  }, [formData, pagination]);
+  }, [form, formData, pagination]);
 
   useEffect(() => {
     search();
   }, [search]);
 
+  const formProps = {
+    form,
+    onFinish,
+  };
+
+  const tableProps = {
+    dataSource,
+    pagination: {
+      ...pagination,
+      total,
+    },
+    loading,
+    onChange,
+  };
+
   return {
-    formProps: {
-      form,
-      onFinish,
-    },
-    tableProps: {
-      dataSource,
-      pagination: {
-        ...pagination,
-        total,
-      },
-      onChange,
-    },
-    formData,
+    formProps,
+    tableProps,
     search,
   };
 };
